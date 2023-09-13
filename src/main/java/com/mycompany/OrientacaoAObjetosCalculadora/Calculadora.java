07f4ad32853dd0a2fd2c332f939236324654c518
THAGOR� OLIVEIRA CAMPOS <thagory.8187@aluno.pr.senac.br>
2023-09-13 15:03:58 -0300

    atualizacao1

--- src/main/java/com/mycompany/OrientacaoAObjetosCalculadora/Calculadora.java
+++ src/main/java/com/mycompany/OrientacaoAObjetosCalculadora/Calculadora.java
@@ -9,5 +9,45 @@ package com.mycompany.OrientacaoAObjetosCalculadora;
  * @author thagory.8187
  */
 public class Calculadora {
+    private float resultado;
+
+    public Calculadora() {
+    }
     
+    //Encapsulamento criado pelo java  alt+insert + gett & sett
+    public float getResultado() {
+        return resultado;
+    }
+    
+    //Encapsulamento criado pelo java  alt+insert + gett & sett
+    public void setResultado(float resultado) {
+        this.resultado = resultado;
+    }
+    
+    //declaracao de parametros
+    public float somar(float a, float b){
+        //setResultado(a + b);       //passagem de parametros
+        return a + b;
+    }
+
+    public float subtrair(float a, float b){
+        //setResultado(a - b);       //passagem de parametros
+        return a - b;
+    }
+
+    public float multiplicar(float a, float b){
+        //setResultado(a * b);       //passagem de parametros
+        return a * b;
+    }
+
+    public float dividir(float a, float b){
+        if (b != 0){
+           // setResultado(a / b);       //passagem de parametros
+           return a / b;
+        }else{
+            System.out.println("Erro: divisão por zero!");
+            //setResultado(Float.NaN);    //NaN = NOT A NUMBER    / Float com F maiusculo e uma classe
+            return Float.NaN;
+        }
+    }
 }
--- src/main/java/com/mycompany/OrientacaoAObjetosCalculadora/ProgramaCalculadora.java
+++ src/main/java/com/mycompany/OrientacaoAObjetosCalculadora/ProgramaCalculadora.java
@@ -11,6 +11,19 @@ package com.mycompany.OrientacaoAObjetosCalculadora;
 public class ProgramaCalculadora {
 
     public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
         
+        float soma, sub, mult, div;
+        
+        soma = calculadora.somar(12, 10);
+        sub = calculadora.subtrair(10, 5);
+        mult = calculadora.multiplicar(10, 10);
+        div = calculadora.dividir(10, 2);
+        //calculadora.multiplicar(2, 10);
+        
+        System.out.println("Resultado da soma: " + soma);
+        System.out.println("Resultado da subtração: " + sub);
+        System.out.println("Resultado da multiplicação: " + mult);
+        System.out.println("Resultado da divisão: " + div);
     }
 }
