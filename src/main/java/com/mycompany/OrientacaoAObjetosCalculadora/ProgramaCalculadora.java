0c6e9007716fad2e409799c8cea09cea63291ac4
THAGOR� OLIVEIRA CAMPOS <thagory.8187@aluno.pr.senac.br>
2023-09-13 14:15:57 -0300

    inicializacao

---
+++ src/main/java/com/mycompany/OrientacaoAObjetosCalculadora/ProgramaCalculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.OrientacaoAObjetosCalculadora;
+
+/**
+ *
+ * @author thagory.8187
+ */
+public class ProgramaCalculadora {
+
+    public static void main(String[] args) {
+        
+    }
+}
